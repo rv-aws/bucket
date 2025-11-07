e31d3d30caf4678def3cd2e8e7f16fe193b0dc91
venerari <venerayan@gmail.com>
2025-11-07 17:38:34 -0500

    update

--- bin/bucket.ts
+++ bin/bucket.ts
@@ -1,20 +1,12 @@
-#!/usr/bin/env node
-import * as cdk from 'aws-cdk-lib/core';
-import { BucketStack } from '../lib/bucket-stack';
+// app-stack.ts
+import { Stack, StackProps } from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import { MyBucket } from '../../reusable-cdk/my-bucket';
 
-const app = new cdk.App();
-new BucketStack(app, 'BucketStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
+export class AppStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
 
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
-
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
-
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
-});
+    new MyBucket(this, 'ReusableBucket', { versioned: false });
+  }
+}
