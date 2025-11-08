18ad0f4947487e9ffd4b3ca0be32b8530bfe7ef6
venerari <venerayan@gmail.com>
2025-11-08 00:40:51 -0500

    update

--- bin/bucket.ts
+++ bin/bucket.ts
@@ -1,12 +1,26 @@
 // app-stack.ts
-import { Stack, StackProps } from 'aws-cdk-lib';
+import { App, Stack, StackProps, RemovalPolicy } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-import { MyBucket } from '../../reusable-cdk/my-bucket';
+import { Bucket, BlockPublicAccess, BucketEncryption } from 'aws-cdk-lib/aws-s3';
 
 export class AppStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    new MyBucket(this, 'ReusableBucket', { versioned: false });
+    new Bucket(this, 'MyExampleBucket', {
+      bucketName: `my-example-bucket-${this.account}-${this.region}`,
+      versioned: true,
+      encryption: BucketEncryption.S3_MANAGED,
+      blockPublicAccess: BlockPublicAccess.BLOCK_ALL,
+      removalPolicy: RemovalPolicy.DESTROY, // Use RETAIN for production
+      autoDeleteObjects: true, // Only works with DESTROY removal policy
+    });    new Bucket(this, 'MyExampleBucket', {
+      bucketName: `my-example-bucket-${this.account}-${this.region}`,
+      versioned: true,
+      encryption: BucketEncryption.S3_MANAGED,
+      blockPublicAccess: BlockPublicAccess.BLOCK_ALL,
+      removalPolicy: RemovalPolicy.DESTROY, // Use RETAIN for production
+      autoDeleteObjects: true, // Only works with DESTROY removal policy
+    });
   }
 }
